731a2e1503df5be0b1c9a13e1e754be8c0505ee3
Matheus Gomes <math.s.gomes@gmail.com>
2024-07-12 23:28:58 -0300

    add register activity validator

--- src/Journey.Application/UseCases/Activities/Register/RegisterActivityValidator.cs
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActivityValidator.cs
@@ -1,6 +1,13 @@
+using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+
 namespace Journey.Application.UseCases.Activities.Register;
 
-public class RegisterActivityValidator
+public class RegisterActivityValidator : AbstractValidator<RequestRegisterActivityJson>
 {
-    
+    public RegisterActivityValidator()
+    {
+        RuleFor(request => request.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+    }
 }
